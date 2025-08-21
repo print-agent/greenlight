42fa3f570049c1a189c1c48ee1f5d4cb58f96e78
print-agent <print_agent_main@proton.me>
2025-08-21 10:36:37 +0500

    Listing all the movies

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -196,5 +196,16 @@ func (app *application) listMoviesHandler(w http.ResponseWriter, r *http.Request
 		return
 	}
 
+	movies, err := app.models.Movies.GetAll(input.Title, input.Genres, input.Filters)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.writeJSON(w, http.StatusOK, envelope{"movies": movies}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+
 	fmt.Fprintf(w, "%+v\n", input)
 }
