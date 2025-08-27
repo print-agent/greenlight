b89d3fc7b0e4c9b406145f4ba0f3778c2bb02d7a
print-agent <print_agent_main@proton.me>
2025-08-27 17:02:32 +0500

    CORs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -6,6 +6,7 @@ import (
 	"flag"
 	"log/slog"
 	"os"
+	"strings"
 	"sync"
 	"time"
 
@@ -37,6 +38,9 @@ type config struct {
 		username string
 		password string
 		sender   string
+	}
+	cors struct {
+		trustedOrigins []string
 	}
 }
 
@@ -76,6 +80,11 @@ func main() {
 	flag.StringVar(&cfg.smtp.username, "smtp-username", "3b82043e9bc2ea", "SMTP username")
 	flag.StringVar(&cfg.smtp.password, "smtp-password", "71716216808c58", "SMTP password")
 	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@printparts.com>", "SMTP sender")
+
+	flag.Func("cors-trusted-origins", "Trusted CORS origins (space separated)", func(val string) error {
+		cfg.cors.trustedOrigins = strings.Fields(val)
+		return nil
+	})
 
 	flag.Parse()
 
