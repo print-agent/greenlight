69505af4abfbd73c191f01cb6056e7b0ab2ef168
print-agent <print_agent_main@proton.me>
2025-08-27 10:04:05 +0500

    Authenticating requests

--- internal/data/user.go
+++ internal/data/user.go
@@ -13,6 +13,8 @@ import (
 
 var ErrDuplicateEmail = errors.New("duplicate email")
 
+var AnonymousUser = &User{}
+
 type User struct {
 	ID        int64     `json:"id"`
 	CreateAt  time.Time `json:"created_at"`
@@ -26,6 +28,10 @@ type User struct {
 type password struct {
 	plaintext *string
 	hash      []byte
+}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
 }
 
 func (p *password) Set(plaintextPassword string) error {
